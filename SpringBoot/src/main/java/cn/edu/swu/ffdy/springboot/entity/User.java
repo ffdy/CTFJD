a02f0dbc6e99cdf28a970b7c3ca210255e83ebed
ffdy <1836017424@qq.com>
2021-12-10 18:23:42 +0800

    修改实体类User

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/entity/User.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/entity/User.java
@@ -1,13 +1,18 @@
 package cn.edu.swu.ffdy.springboot.entity;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Table(name = "Users")
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +22,17 @@ public class User {
     private String email;
     private String type;
     private Date created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
