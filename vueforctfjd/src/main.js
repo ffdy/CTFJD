48ada8660adaa3c5213b00831174e5bae0017c2c
ffdy <1836017424@qq.com>
2021-12-10 17:11:54 +0800

    创建Vue3项目 添加插件element 添加插件axios

---
+++ vueforctfjd/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
+import './styles.scss'
+
+Vue.use(ElementUI, { locale })
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
