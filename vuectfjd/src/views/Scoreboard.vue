a67606b369f678f2c8a3c64096154a750ab91693
ffdy <1836017424@qq.com>
2021-12-11 14:05:49 +0800

    积分展示和筛选

---
+++ vuectfjd/src/views/Scoreboard.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-table
+      :data="tableData.filter(data => !search
+      || data.email.toLowerCase().includes(search.toLowerCase())
+      || data.name.toLowerCase().includes(search.toLowerCase()))"
+      style="width: 100%">
+    <el-table-column
+        label="Place"
+        prop="place">
+    </el-table-column>
+    <el-table-column
+        label="Name"
+        prop="name">
+    </el-table-column>
+    <el-table-column
+        label="Email"
+        prop="email">
+
+    </el-table-column>
+    <el-table-column
+        align="right">
+      <template slot="header" slot-scope="scope">
+        <el-input
+            v-model="search"
+            size="mini"
+            placeholder="输入关键字搜索"/>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [{
+        name: '王小虎',
+        email: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        email: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        email: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        email: '上海市普陀区金沙江路 1516 弄'
+      }],
+      search: ''
+    }
+  },
+  methods: {
+    created() {
+
+    }
+  },
+}
+</script>
