2b45eb73a9d9112de77ed65ffa5ff1f02cf04e12
ffdy <1836017424@qq.com>
2021-12-15 02:10:15 +0800

    添加User表相关的操作

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/controller/UsersHandler.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/controller/UsersHandler.java
@@ -2,12 +2,14 @@ package cn.edu.swu.ffdy.springboot.controller;
 
 
 import cn.edu.swu.ffdy.springboot.entity.User;
+import cn.edu.swu.ffdy.springboot.entity.UserInfo;
 import cn.edu.swu.ffdy.springboot.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -22,18 +24,64 @@ public class UsersHandler {
     }
 
     @GetMapping("/findAll/{page}")
-    public Page<User> findAllUsers(@PathVariable("page")Integer page) {
+    public Page<UserInfo> findAllUsers(@PathVariable("page")Integer page) {
         PageRequest request = PageRequest.of(page, 10);
-        return usersRepository.findAll(request);
+        return usersRepository.findAllUserInfo(request);
+    }
+
+    @PostMapping("/findByName")
+    public User getUserByName(@RequestBody String name) {
+        return usersRepository.findByName(name);
     }
 
     @PostMapping("/create")
     public String createUser(@RequestBody User user) {
+        User oldUser = usersRepository.findByName(user.getName());
+        if(oldUser != null) {
+            return "用户 " + user.getName() + " 已经存在";
+        }
+        oldUser = usersRepository.findByEmail(user.getEmail());
+        if(oldUser != null) {
+            return "邮箱 " + user.getEmail() + " 已被绑定";
+        }
+        user.setType("user");
+        user.setCreated(new Date());
         User newUser = usersRepository.save(user);
         if(newUser != null) {
-            return "success";
+            return "创建成功";
         } else {
-            return "fail";
+            return "创建失败";
         }
+    }
+
+    @PostMapping("/edit")
+    public String editUser(@RequestBody User user, @RequestBody String newPass) {
+        User oldUser = usersRepository.findById(user.getId()).get();
+        if(oldUser != null) {
+            if(!user.getName().equals(oldUser.getName())
+                    || !user.getEmail().equals(oldUser.getEmail())
+                    || !user.getPassword().equals(oldUser.getPassword())) {
+                return "ERROR";
+            }
+            user.setPassword(newPass);
+            if(usersRepository.save(user) != null) {
+                return "创建成功";
+            } else {
+                return "创建失败";
+            }
+        } else {
+            return "ERROR";
+        }
+    }
+
+    @PostMapping("/login")
+    public UserInfo login(@RequestBody User user) {
+        User oldUser = usersRepository.findByName(user.getName());
+        if(oldUser != null) {
+            if(oldUser.getPassword().equals(user.getPassword())) {
+                return new UserInfo(oldUser.getName(), oldUser.getEmail());
+            }
+        }
+        return null;
     }
 }
---
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/entity/UserInfo.java
@@ -0,0 +1,19 @@
+package cn.edu.swu.ffdy.springboot.entity;
+
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserInfo {
+    private String name;
+    private String email;
+
+    public UserInfo(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
