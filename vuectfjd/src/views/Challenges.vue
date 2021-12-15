edb87e5adfed87b063378df305c4e33582729e34
ffdy <1836017424@qq.com>
2021-12-15 16:53:31 +0800

    完善前端题目展示

--- vuectfjd/src/views/Challenges.vue
+++ vuectfjd/src/views/Challenges.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
     <el-row>
-      <el-col :span="3" v-for="(o, index) in challenges" :key="o" :offset="index%6 >0 ? 1:0">
+      <el-col :span="3" v-for="(o, index) in challenges" :key="index" :offset="index%6 >0 ? 1:0">
         <el-button type="primary"
                    style="width: 100%; height: 120px; margin-bottom: 10%"
-                   @click="dialogTableVisible = true">
+                   @click="dialogTableVisible = true;currentChallenge = o;flag = ''">
           <h3>{{o.name}}</h3>
-          <h3>{{o.scores}}</h3>
+          <h3>{{o.value}}</h3>
+          <h3>{{o.category}}</h3>
         </el-button>
         <el-dialog :visible.sync="dialogTableVisible">
           <template>
             <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
               <el-tab-pane label="Challenge" name="first">
                 <div align="center">
-                  <h1>{{o.name}}</h1>
-                  <h2>{{o.scores}}</h2>
-                  <h4>{{o.description}}</h4>
-                  <el-input style="width: 70%"></el-input>
-                  <el-button>asdf</el-button>
+                  <h1>{{currentChallenge.name}}</h1>
+                  <h2>{{currentChallenge.value}}</h2>
+                  <h4>{{currentChallenge.description}}</h4>
+                  <el-input style="width: 70%" v-model="flag"></el-input>
+                  <el-button @click="submitFlag">提交</el-button>
                 </div>
               </el-tab-pane>
               <el-tab-pane label="Solved" name="second">
@@ -50,64 +51,44 @@
 export default {
   data() {
     return {
-      challenges: [
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-        {
-          name: 'challenges name',
-          description: 'challenge desc',
-          scores: '300'
-        },
-      ],
+      challenges: [],
       activeName: 'first',
-      dialogTableVisible: false
+      dialogTableVisible: false,
+      currentChallenge: {},
+      flag: '',
     };
   },
   methods: {
+    handleClick(msg) {
+      console.log(msg)
+    },
+    submitFlag() {
+      const _this = this
+      if(this.flag!='') {
+        console.log(this.flag)
+        axios.post("http://localhost:8181/flag/check/"+this.currentChallenge.id, this.flag).then(function (resp){
+          console.log(resp)
+          _this.$message(resp.data)
+          if(resp.data === "flag正确") {
 
+          }
+        })
+      } else {
+        this.$message("flag不能为空")
+      }
+    },
+    getChallengeStatus() {
+      const _this = this
+      axios.get("http://localhost:8181/challenge")
+    }
+  },
+  created() {
+    const _this = this
+    axios.get("http://localhost:8181/challenge/findAll").then(function (resp) {
+      console.log(resp.data)
+      _this.challenges = resp.data
+      _this.getChallengeStatus()
+    })
   }
 }
 </script>
