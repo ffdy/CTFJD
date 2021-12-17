d604fac4c9d095e06a229543819b593c43dcb9cf
ffdy <1836017424@qq.com>
2021-12-18 01:45:18 +0800

    改正接口

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/ChallengeRepository.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/ChallengeRepository.java
@@ -4,5 +4,4 @@ import cn.edu.swu.ffdy.springboot.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Integer> {
-    Integer findByChallengeId(Integer challengeId);
 }
