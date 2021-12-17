e0ff339bd762bb75eddf17fc97dedc9d2becb92a
ffdy <1836017424@qq.com>
2021-12-18 01:41:13 +0800

    flag验证并将提交信息和解题信息分别写入数据库

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/controller/FlagHandler.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/controller/FlagHandler.java
@@ -1,8 +1,12 @@
 package cn.edu.swu.ffdy.springboot.controller;
 
 import cn.edu.swu.ffdy.springboot.entity.Flag;
+import cn.edu.swu.ffdy.springboot.entity.Solve;
+import cn.edu.swu.ffdy.springboot.entity.Submission;
 import cn.edu.swu.ffdy.springboot.repository.FlagRepository;
 import cn.edu.swu.ffdy.springboot.repository.SolveRepository;
+import cn.edu.swu.ffdy.springboot.repository.SubmissionRepository;
+import cn.edu.swu.ffdy.springboot.utils.SessionContents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/flag")
@@ -18,6 +23,8 @@ public class FlagHandler {
     FlagRepository flagRepository;
     @Autowired
     SolveRepository solveRepository;
+    @Autowired
+    SubmissionRepository submissionRepository;
 
     @PostMapping("/check/{challengeId}")
     public String checkFlag(@PathVariable("challengeId") Integer id,
@@ -29,11 +36,37 @@ public class FlagHandler {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        if(currentFlag!=null && (currentFlag.getContent()+'=').equals(flag)) {
-            HttpSession session = request.getSession(true)
-            return "flag正确";
+
+        // 去掉多余的等号
+        flag = flag.substring(0,flag.length()-1);
+        String flagStatus;
+
+        HttpSession session = request.getSession(true);
+        if(currentFlag!=null && currentFlag.getContent().equals(flag)) {
+            flagStatus = "correct";
         } else {
-            return "flag错误"+flag;
+            flagStatus = "incorrect";
         }
+
+        Submission submission = new Submission();
+        submission.setChallengeId(id);
+        submission.setDate(new Date());
+        submission.setUserId((Integer) session.getAttribute(SessionContents.ID));
+        submission.setProvided(flag);
+        submission.setIp(request.getRemoteAddr());
+        submission.setType(flagStatus);
+        submissionRepository.save(submission);
+        System.out.println(submission);
+
+        Integer userId = (Integer) session.getAttribute(SessionContents.ID);
+        Solve solve = solveRepository.findByChallengeIdAndUserId(id, userId);
+        if(solve==null) {
+            Solve newSolve = new Solve();
+            newSolve.setId(submission.getId());
+            newSolve.setChallengeId(id);
+            newSolve.setUserId(userId);
+            solveRepository.save(newSolve);
+        }
+        return flagStatus;
     }
 }
