b6c07b0bfbf7b9cadb684ac4c8d173d6b5e9c0ef
ffdy <1836017424@qq.com>
2021-12-18 01:37:40 +0800

    添加通过challengeId查询challenge信息的接口

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/ChallengeRepository.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/ChallengeRepository.java
@@ -4,4 +4,5 @@ import cn.edu.swu.ffdy.springboot.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Integer> {
+    Integer findByChallengeId(Integer challengeId);
 }
