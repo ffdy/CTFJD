c01dd64e920a3390c958ed8b437fcb0a33389484
ffdy <1836017424@qq.com>
2021-12-18 01:46:22 +0800

    添加解题查询接口

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/SolveRepository.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/SolveRepository.java
@@ -3,5 +3,10 @@ package cn.edu.swu.ffdy.springboot.repository;
 import cn.edu.swu.ffdy.springboot.entity.Solve;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SolveRepository extends JpaRepository<Solve, Integer> {
+    Solve findByChallengeIdAndUserId(Integer ChallengeId, Integer userId);
+
+    List<Solve> findByUserId(Integer userId);
 }
