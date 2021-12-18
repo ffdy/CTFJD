11b5641f13018dc284c4ee5b1192b78f23cb63c9
ffdy <1836017424@qq.com>
2021-12-19 02:41:24 +0800

    添加个人提交的查询和对应的Jpa接口

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/controller/SubmissionHandler.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/controller/SubmissionHandler.java
@@ -2,12 +2,15 @@ package cn.edu.swu.ffdy.springboot.controller;
 
 import cn.edu.swu.ffdy.springboot.entity.Submission;
 import cn.edu.swu.ffdy.springboot.repository.SubmissionRepository;
+import cn.edu.swu.ffdy.springboot.utils.SessionContents;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -19,5 +22,12 @@ public class SubmissionHandler {
     @GetMapping("/findAll")
     public List<Submission> findAll() {
         return submissionRepository.findAll();
+    }
+
+    @GetMapping("/mysubmission")
+    public List<Submission> mySubmission(HttpServletRequest request) {
+        HttpSession session = request.getSession(true);
+        Integer userId = (Integer) session.getAttribute(SessionContents.ID);
+        return submissionRepository.findByUserId(userId);
     }
 }
--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/SubmissionRepository.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/repository/SubmissionRepository.java
@@ -3,5 +3,8 @@ package cn.edu.swu.ffdy.springboot.repository;
 import cn.edu.swu.ffdy.springboot.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubmissionRepository extends JpaRepository<Submission, Integer> {
+    List<Submission> findByUserId(Integer userId);
 }
