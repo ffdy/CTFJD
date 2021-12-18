41af88a7afc8a6a14c455af61940ddfd4cdc0117
ffdy <1836017424@qq.com>
2021-12-19 02:39:51 +0800

    去除多余代码

--- vuectfjd/src/views/Users.vue
+++ vuectfjd/src/views/Users.vue
@@ -30,24 +30,7 @@
 export default {
   data() {
     return {
-      tableData: [{
-        name: '王小虎',
-        email: '上海市普陀区金沙江路 1518 弄',
-        place: 1,
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        email: '上海市普陀区金沙江路 1517 弄',
-        place: 2,
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        email: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        email: '上海市普陀区金沙江路 1516 弄'
-      }],
+      tableData: [],
       search: ''
     }
   },
