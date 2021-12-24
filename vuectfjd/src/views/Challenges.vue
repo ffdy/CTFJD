bacc237992fea0fc9ecfd639b8a793bccc15042a
ffdy <1836017424@qq.com>
2021-12-25 00:35:18 +0800

    优化显示 完善解题人显示功能

--- vuectfjd/src/views/Challenges.vue
+++ vuectfjd/src/views/Challenges.vue
@@ -4,14 +4,14 @@
       <el-col :span="3" v-for="(o, index) in challenges" :key="index" :offset="index%6 >0 ? 1:0">
         <el-button type="primary"
                    style="width: 100%; height: 120px; margin-bottom: 10%"
-                   @click="dialogTableVisible = true;currentChallenge = o;openChallenge">
+                   @click="openChallenge(o)">
           <h3>{{o.name}}</h3>
           <h3>{{o.value}}</h3>
           <h3>{{o.category}}</h3>
         </el-button>
         <el-dialog :visible.sync="dialogTableVisible">
           <template>
-            <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+            <el-tabs v-model="activeName" type="card">
               <el-tab-pane label="Challenge" name="first">
                 <div align="center">
                   <h1>{{currentChallenge.name}}</h1>
@@ -27,12 +27,17 @@
                     height="300px"
                     style="width: 100%">
                   <el-table-column
+                      type="index"
+                      width="70"
+                      label="Index">
+                  </el-table-column>
+                  <el-table-column
                       label="Name"
                       prop="name">
                   </el-table-column>
                   <el-table-column
-                      label="Time"
-                      prop="time">
+                      label="Email"
+                      prop="email">
                   </el-table-column>
                 </el-table>
               </el-tab-pane>
@@ -44,9 +49,6 @@
   </div>
 </template>
 
-<style>
-</style>
-
 <script>
 export default {
   data() {
@@ -55,20 +57,15 @@ export default {
       activeName: 'first',
       dialogTableVisible: false,
       currentChallenge: {},
-      solves: {},
+      solves: [],//数组
       flag: '',
     };
   },
   methods: {
-    handleClick(msg) {
-      console.log(msg)
-    },
     submitFlag() {
       const _this = this
       if(this.flag!=='') {
-        console.log(this.flag)
         axios.post("http://localhost:8181/flag/check/"+this.currentChallenge.id, this.flag).then(function (resp){
-          console.log(resp)
           _this.$message(resp.data)
           if(resp.data === "correct") {
             _this.$router.push("/challenges")
@@ -78,16 +75,13 @@ export default {
         this.$message("flag不能为空")
       }
     },
-    getChallengeStatus() {
-      // const _this = this
-    },
-    openChallenge(msg) {
-      // dialogTableVisible = true;currentChallenge = o;
-      console.log(msg)
+    openChallenge(o) {
+      this.dialogTableVisible = true
+      this.currentChallenge = o
+      this.activeName = 'first'
       this.flag = ''
       const _this = this
-      axios.get("http://localhost:8181/solve/findSolves/"+this.currentChallenge.id).then(function (resp) {
-        console.log(resp)
+      axios.get("http://localhost:8181/solve/findbychallengeid/"+this.currentChallenge.id).then(function (resp) {
         _this.solves = resp.data
       })
     }
@@ -95,9 +89,7 @@ export default {
   created() {
     const _this = this
     axios.get("http://localhost:8181/challenge/findAll").then(function (resp) {
-      console.log(resp.data)
       _this.challenges = resp.data
-      _this.getChallengeStatus()
     })
   }
 }
