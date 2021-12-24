c1a608c0d4924af7ee1d3ddd619aa9aee27ed29c
ffdy <1836017424@qq.com>
2021-12-25 00:36:24 +0800

    添加验证码显示组件

--- vuectfjd/src/views/Login.vue
+++ vuectfjd/src/views/Login.vue
@@ -1,11 +1,21 @@
 <template>
   <div align="center" style="margin-top: 150px">
-    <el-form style="width: 60%" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px" class="demo-ruleForm">
+    <el-form style="width: 60%" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px"
+             class="demo-ruleForm">
       <el-form-item label="User Name" prop="name">
         <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
       <el-form-item label="Password" prop="pass">
         <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="Validate Code" prop="validateCode">
+        <el-col :span="16">
+          <el-input v-model="ruleForm.validateCode" autocomplete="off" size="60%"></el-input>
+        </el-col>
+        <el-col :span="8">
+          <el-image :src="validateUrl" style="height: 40px;"/>
+        </el-col>
+
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
@@ -16,20 +26,24 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
+      validateUrl: 'http://localhost:8181/validate',
       ruleForm: {
         name: '',
         pass: '',
+        validateCode: '',
       },
       rules: {
         name: [
-          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {required: true, message: '请输入用户名', trigger: 'blur'},
         ],
         pass: [
-          { required: true, message: '请输入密码',  trigger: 'blur' }
+          {required: true, message: '请输入密码', trigger: 'blur'}
+        ],
+        validateCode: [
+          {required: true, message: '请输入验证码', trigger: 'blur'}
         ],
       }
     };
@@ -41,18 +55,21 @@ export default {
         if (valid) {
           const user = {
             name: this.ruleForm.name,
-            password: this.ruleForm.pass
+            password: this.ruleForm.pass,
+            validate: this.ruleForm.validateCode
           }
+          console.log(user)
           axios.post("http://localhost:8181/user/login", user).then(function (resp) {
             console.log(resp)
-            if(resp.data != '') {
+            if (resp.data != '') {
               _this.$message("login success")
               _this.$cookies.set("login", '1')
               _this.$cookies.set("user", resp.data)
               _this.$emit("getUserInfo", resp.data)
               _this.$router.push("/")
             } else {
-              _this.$message("用户名或密码错误")
+              _this.$message("验证码或用户名或密码错误")
+              window.location.reload()
             }
           })
         } else {
