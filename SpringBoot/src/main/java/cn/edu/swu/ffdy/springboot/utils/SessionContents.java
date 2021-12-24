0662b1c6960abe7171c1358d82bf6dd3b8d9c765
ffdy <1836017424@qq.com>
2021-12-25 00:43:38 +0800

    添加登录状态的字段为登录拦截器

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/utils/SessionContents.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/utils/SessionContents.java
@@ -5,4 +5,5 @@ public class SessionContents {
     public static final String USER_NAME = "USER_NAME";
     public static final String USER_TYPE = "USER_TYPE";
     public static final String USER_VALIDATE_CODE = "USER_VALIDATE_CODE";
+    public static final String LOGIN_STATUS = "LOGIN_STATUS";
 }
