77384c9de6d27516b4ec83e87b4163f976a2b71b
ffdy <1836017424@qq.com>
2021-12-30 01:09:08 +0800

    完善靶机启动的相关组件和接口绑定 添加文件下载的相关组件和接口绑定

--- vuectfjd/src/views/Challenges.vue
+++ vuectfjd/src/views/Challenges.vue
@@ -1,24 +1,42 @@
 <template>
   <div>
     <el-row>
-      <el-col :span="3" v-for="(o, index) in challenges" :key="index" :offset="index%6 >0 ? 1:0">
+      <el-col :span="5" v-for="(o, index) in challenges" :key="index" :offset="index%4 >0 ? 1:0">
         <el-button type="primary"
                    style="width: 100%; height: 120px; margin-bottom: 10%"
-                   @click="openChallenge(o)">
-          <h3>{{o.name}}</h3>
-          <h3>{{o.value}}</h3>
-          <h3>{{o.category}}</h3>
+                   @click="openChallenge(o, index)">
+          <h3>{{ o.name }}</h3>
+          <h3>{{ o.value }}</h3>
+          <h3>{{ o.category }}</h3>
         </el-button>
         <el-dialog :visible.sync="dialogTableVisible">
           <template>
-            <el-tabs v-model="activeName" type="card">
+            <el-tabs v-model="activeName" type="card"
+                     v-loading="(currentImage.loading && (currentChallenge.id === currentImage.id))">
               <el-tab-pane label="Challenge" name="first">
                 <div align="center">
-                  <h1>{{currentChallenge.name}}</h1>
-                  <h2>{{currentChallenge.value}}</h2>
-                  <h4>{{currentChallenge.description}}</h4>
-                  <el-input style="width: 70%" v-model="flag"></el-input>
-                  <el-button @click="submitFlag">提交</el-button>
+                  <h1>{{ currentChallenge.name }}</h1>
+                  <h2>{{ currentChallenge.value }}</h2>
+                  <h4>{{ currentChallenge.description }}</h4>
+                  <div v-if="currentChallenge.type === 'dynamic_docker'">
+                    <div v-if="currentImage.loaded && (currentImage.id === currentChallenge.id)">
+                      <h3>{{ currentImage.url }}</h3>
+                      <el-button type="warning" @click="restartDockerImage" plain>重启靶机</el-button>
+                      <el-button type="danger" @click="stopDockerImage" plain>销毁靶机</el-button>
+                    </div>
+                    <div v-else>
+                      <el-button type="success" @click="startDockerImage" plain>启动靶机</el-button>
+                    </div>
+                  </div>
+                  <div v-if="currentFile.id === currentChallenge.id" style="margin-top: 20px;">
+                    <el-button icon="el-icon-download" size="small" type="info" v-for="(file, file_index) in currentFile.files"
+                               :key="file_index" @click="downloadFile(file)">{{file.name}}
+                    </el-button>
+                  </div>
+                  <div style="margin-top: 20px;">
+                    <el-input style="width: 70%" v-model="flag"></el-input>
+                    <el-button @click="submitFlag">提交</el-button>
+                  </div>
                 </div>
               </el-tab-pane>
               <el-tab-pane label="Solved" name="second">
@@ -57,6 +75,16 @@ export default {
       activeName: 'first',
       dialogTableVisible: false,
       currentChallenge: {},
+      currentImage: {
+        id: null,
+        url: '',
+        loading: false,
+        loaded: false,
+      },
+      currentFile: {
+        id: null,
+        files: [],
+      },
       solves: [],//数组
       flag: '',
     };
@@ -64,10 +92,10 @@ export default {
   methods: {
     submitFlag() {
       const _this = this
-      if(this.flag!=='') {
-        axios.post("http://localhost:8181/flag/check/"+this.currentChallenge.id, this.flag).then(function (resp){
+      if (this.flag !== '') {
+        axios.post("http://localhost:8181/flag/check/" + this.currentChallenge.id, this.flag).then(function (resp) {
           _this.$message(resp.data)
-          if(resp.data === "correct") {
+          if (resp.data === "correct") {
             _this.$router.push("/challenges")
           }
         })
@@ -75,20 +103,56 @@ export default {
         this.$message("flag不能为空")
       }
     },
-    openChallenge(o) {
+    openChallenge(o, index) {
       this.dialogTableVisible = true
       this.currentChallenge = o
       this.activeName = 'first'
       this.flag = ''
       const _this = this
-      axios.get("http://localhost:8181/solve/findbychallengeid/"+this.currentChallenge.id).then(function (resp) {
+      axios.get("http://localhost:8181/solve/findbychallengeid/" + this.currentChallenge.id).then(function (resp) {
         _this.solves = resp.data
       })
-    }
+      axios.get("http://localhost:8181/challengefile/getinfo/" + this.currentChallenge.id).then(function (resp) {
+        _this.currentFile.id = _this.currentChallenge.id
+        _this.currentFile.files = resp.data
+      })
+    },
+    startDockerImage() {
+      this.currentImage.loading = true
+      this.currentImage.id = this.currentChallenge.id
+      const _this = this
+      axios.get("http://localhost:8181/docker/geturl/" + this.currentChallenge.id).then(function (resp) {
+        console.log(resp.data)
+        _this.currentImage.loading = false;
+        _this.currentImage.loaded = true;
+        _this.currentImage.url = resp.data
+      })
+    },
+    restartDockerImage() {
+      this.currentImage.loading = true;
+      const _this = this
+      axios.get("http://localhost:8181/docker/restart").then(function (resp) {
+        console.log(resp.data)
+        _this.currentImage.loading = false;
+      })
+    },
+    stopDockerImage() {
+      this.currentImage.loading = true;
+      const _this = this
+      axios.get("http://localhost:8181/docker/stop").then(function (resp) {
+        console.log(resp.data)
+        _this.currentImage.loaded = false;
+        _this.currentImage.loading = false;
+      })
+    },
+    downloadFile(file) {
+      window.location.href = "http://localhost:8181/challengefile/getfile/" + file.challengeId + "/" +file.name
+    },
   },
   created() {
     const _this = this
     axios.get("http://localhost:8181/challenge/findAll").then(function (resp) {
+      console.log(resp.data)
       _this.challenges = resp.data
     })
   }
