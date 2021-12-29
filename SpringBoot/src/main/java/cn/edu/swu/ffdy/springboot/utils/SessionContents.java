789d07eedc86f98cfaa783244bdffaaf1d458500
ffdy <1836017424@qq.com>
2021-12-30 01:15:53 +0800

    添加docker容器记录的相关Session记录

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/utils/SessionContents.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/utils/SessionContents.java
@@ -6,4 +6,7 @@ public class SessionContents {
     public static final String USER_TYPE = "USER_TYPE";
     public static final String USER_VALIDATE_CODE = "USER_VALIDATE_CODE";
     public static final String LOGIN_STATUS = "LOGIN_STATUS";
+    public static final String DOCKER_ID = "DOCKER_ID";
+    public static final String EXPOSE_PORT = "EXPOSE_PORT";
+    public static final String DYNAMIC_FLAG = "DYNAMIC_FLAG";
 }
