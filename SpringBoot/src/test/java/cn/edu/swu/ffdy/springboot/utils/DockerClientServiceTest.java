3bca461e9e3c2323e6bf2d93c5959a2a1e30c0d7
ffdy <1836017424@qq.com>
2021-12-30 01:13:02 +0800

    参数修改

--- SpringBoot/src/test/java/cn/edu/swu/ffdy/springboot/utils/DockerClientServiceTest.java
+++ SpringBoot/src/test/java/cn/edu/swu/ffdy/springboot/utils/DockerClientServiceTest.java
@@ -2,6 +2,7 @@ package cn.edu.swu.ffdy.springboot.utils;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.RestartContainerCmd;
 import org.junit.jupiter.api.Test;
 
 import javax.print.Doc;
@@ -18,7 +19,7 @@ class DockerClientServiceTest {
 
     @Test
     public void createImage() throws SocketException {
-        CreateContainerResponse image = DockerClientService.createContainers("swuctf/web1", 80);
+        CreateContainerResponse image = DockerClientService.createContainers("swuctf/web1", 80, 5555);
         System.out.println(image);
         DockerClientService.startContainer(image.getId());
     }
@@ -26,5 +27,16 @@ class DockerClientServiceTest {
     @Test
     public void startImage() {
         DockerClientService.startContainer("457a6d45377e");
+    }
+
+    @Test
+    public void restartImage() {
+        DockerClientService.restartContainer("cf2361abea82");
+    }
+
+    @Test
+    public void stopImage() {
+        DockerClientService.stopContainer("cf2361abea82");
+        DockerClientService.removeContainer("cf2361abea82");
     }
 }
