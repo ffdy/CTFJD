74aa70e24826ff3d2cabaf332a1cb856a66ee4ba
ffdy <1836017424@qq.com>
2021-12-30 01:10:59 +0800

    添加文件名字段 修改字段名便于Jpa查询

--- SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/entity/ChallengeFile.java
+++ SpringBoot/src/main/java/cn/edu/swu/ffdy/springboot/entity/ChallengeFile.java
@@ -3,6 +3,7 @@ package cn.edu.swu.ffdy.springboot.entity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -10,11 +11,14 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@ToString
 @RequiredArgsConstructor
 public class ChallengeFile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String name;
     private String location;
-    private Integer challenge_id;
+    @Column(name = "challenge_id")
+    private Integer challengeId;
 }
